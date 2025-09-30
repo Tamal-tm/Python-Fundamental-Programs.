a202acd71e0389dae2d15654d825894f208295bd
Tamal Mallick <tamalmallick23@gmail.com>
2025-09-30 12:53:11 +0530

    Add files via upload

---
+++ Empty_List.py
@@ -0,0 +1,23 @@
+# Using boolean operation
+
+print(bool([]))
+
+print(bool([4,5]))
+
+my_list=[]
+if not my_list: # True
+    print("The list is empty.")
+
+# Using Len function
+
+my_list2=[]
+if len(my_list2)==0:
+    print('List is empty.')
+else:
+    print(my_list2)
+
+# Using square brackets
+
+my_list3=[]
+if my_list3 == []:
+    print("Empty")
