61198dc628ba86b75036bcc62d6318c3fd4e80ce
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-05 16:24:41 +0530

    Update Shuffle_Cards.py

--- Shuffle_Cards.py
+++ Shuffle_Cards.py
@@ -1,12 +1,1 @@
-import random, itertools # Modules for shuffle. 
-                         # To obtan each cars, we will use itertools.
 
-deck=list(itertools.product(range(1,14),["Spade","Club",'Hearts',"Diamond"]))
-print(deck) # Will print a list of deck tuples. Ordered.
-
-random.shuffle(deck)
-
-print(deck)
-
-for i in range(5):
-    print(deck[i][0], "of", deck[i][1])
