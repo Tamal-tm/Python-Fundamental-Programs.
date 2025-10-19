212a124ed9d1f15a174e947ecca4dcd5b79bb5ca
Tamal Mallick <tamalmallick23@gmail.com>
2025-10-19 07:55:11 +0530

    Update Comprehension.py

--- Comprehension.py
+++ Comprehension.py
@@ -4,6 +4,7 @@ a = ['iron','hulk','vision','thor']
 
 new_list=[i.upper() for i in a]
 print(new_list)
+
 m = [i for i in range(10)]
 print(m)
 
@@ -12,3 +13,4 @@ k=[1,2,3,4,5,6,7,8,9]
 m = [i for i in range(10) if i% 2 == 0]
 
 print(m)
+
